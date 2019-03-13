de296ba4e9c095100678c0f04e78c5e4c6e8ddfe
felic <felice.trotta@studenti.unicampania.it>
2019-03-13 17:02:19 +0100

    secondo commit

--- app/src/main/java/com/example/secondalezione/MainActivity.kt
+++ app/src/main/java/com/example/secondalezione/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.secondalezione
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// classe per la main activity
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = 18
+        val b = 18
+
     }
 }
